d7c0d74e5b280ba7b5dd893625f02a2403274f39
Herika <herikarajput3@gmail.com>
2026-05-30 12:31:25 +0530

    refactor: centralize database connection error logging

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import logger from "../utils/logger";
 
 const connectDb = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         // console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(error);
+        logger.log(error);
         // process.exit(1); // it is good for production not for testing
         throw error;
     }
